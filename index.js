96a3123df455e86175e11763eb08395a592005b5
Harshil <ghagrawal17@gmail.com>
2023-02-11 00:22:09 +0100

    ⚡ Update discord.js and dotenv version

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-const bot = new Discord.Client();
+const bot = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+// Make sure to grant the Message Content Intent Gateway Intents Privilege to your bot
+
+// Execute once when the bot is ready
+bot.once(Events.ClientReady, (c) => {
+	console.log(`Bot is ready! Logged in as ${c.user.tag}`);
+});
 
 bot.login(`${process.env.BOT_TOKEN}`);
